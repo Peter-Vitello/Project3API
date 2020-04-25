eaca2e731f1ebe2ae5cfa4971886ee59a6887a56
RoboDogPete <61554702+RoboDogPete@users.noreply.github.com>
2020-04-25 16:03:15 -0400

    got rid of a buch of unneeded stuff

--- src/main.js
+++ src/main.js
@@ -1,33 +1,8 @@
-/*
-	main.js is primarily responsible for hooking up the UI to the rest of the application 
-	and setting up the main event loop
-*/
-
-// We will write the functions in this file in the traditional ES5 way
-// In this instance, we feel the code is more readable if written this way
-// If you want to re-write these as ES6 arrow functions, to be consistent with the other files, go ahead!
-// If you want to re-write these as ES6 arrow functions, to be consistent with the other files, go ahead!
-import * as utils from './utils.js';
-const drawParams = {
-    showGradient  : true,
-    showRects     : true,
-    showCircles   : true,
-    showNoise     : false,
-    showInvert    : false,
-    showEmboss    : false,
-    showCurve     : false
-};
-
-// 1 - here we are faking an enumeration
-const DEFAULTS = Object.freeze({
-	
-});
-
 function init(){
 	setupUI();
 }
 
-function setupUI(canvasElement){
+function setupUI(){
   // A - hookup fullscreen button
     const fsButton = document.querySelector("#fsButton");
     const playButton = document.querySelector("#playButton");
@@ -49,22 +24,6 @@ function setupUI(canvasElement){
         window.open(string);
    }   
 
-}
-
-function loop(){
-/* NOTE: This is temporary testing code that we will delete in Part II */
-	requestAnimationFrame(loop);
-    
-    canvas.draw(drawParams);
-    audio.gainNode.gain.value = control.volume/100;
-    canvas.ChangeBarHeight(control.barheight/100);
-    drawParams.showGradient = control.gradient; 
-    drawParams.showCircles = control.circle; 
-    drawParams.showNoise = control.noise; 
-    drawParams.showInvert = control.invertColors; 
-    drawParams.showEmboss = control.emboss; 
-    drawParams.showCurve = control.beizerCurve; 
-    
 }
 
 export {init};
