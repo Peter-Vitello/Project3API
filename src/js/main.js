725334048cfe23a67f7ae6fa9a3c0508c3867381
Peter Vitello <pxv6460@g.rit.edu>
2020-05-01 11:36:37 -0400

    got rid of some unneeded things and added comments

--- src/js/main.js
+++ src/js/main.js
@@ -43,44 +43,20 @@ function setupUI(){
         //"window.location.href = 'https://www.w3docs.com';" value="w3docs"
         console.log(concat);
    }
+    // resets the text in the boxed and clears local storage
     resetButton.onclick = e => {
         localStorage.clear();
         myOtherURL.value = "Must enter URL here first. Needs to have .jpg, .png, or .jpeg at the end.";
         textInBox.value = "";
    }
+    // opens a new tab on browser for images that the api can use 
     goToGoogleButton.onclick = e => {
         window.open('https://www.google.com/search?rlz=1C1GCEU_enUS894US894&sxsrf=ALeKk00u-7UQANAt-xwqAboQStV7w1TGlg:1588343534312&source=univ&tbm=isch&q=jpg+text+images&hl=en&sa=X&ved=2ahUKEwiY75Lh8JLpAhUBnKwKHT1DBKMQsAR6BAgKEAE&biw=1920&bih=969', '_blank');
    }
+    // takes the image from the url and finds text on the image and displays it in the textbox
     photoButton.onclick = e => 
     {
         let myURL = 'https://api.ocr.space/parse/imageurl?apikey=2edc10ce1188957&url=' + encodeURIComponent(myOtherURL.value);
-        //let myURL = '{ "name":"John", "age":30, "city":"New York"}';
-        //let obj = getJSON
-        
-        //'{"ParsedResults": [{"TextOverlay": {"Lines": [  ],"HasOverlay": false,"Message": "Text overlay is not provided as it is not requested"},"TextOrientation": "0","FileParseExitCode": 1,"ParsedText": "GAVE AWAY MY BOOK\r\nFREE ON\r\nREACHED TOP OF AMAZON\r\nBESTSELLER\r\n","ErrorMessage": "","ErrorDetails": ""}],"OCRExitCode": 1,"IsErroredOnProcessing": false,"ProcessingTimeInMilliseconds": "3010","SearchablePDFURL": "Searchable PDF not generated as it was not requested."}'
-        //let getJSON = function(myURL, callback) {
-        //    let xhr = new XMLHttpRequest();
-        //    xhr.open('GET', myURL, true);
-        //    xhr.responseType = 'json';
-        //    xhr.onload = function() {
-        //        let status = xhr.status;
-        //        if (status === 200) {
-        //            callback(null, xhr.response);
-        //        } else {
-        //            callback(status, xhr.response);
-        //        }
-        //    };
-        //    xhr.send();
-        //};
-//
-        //getJSON(myURL,
-        //function(err, data) {
-        //if (err !== null) {
-        //    alert('Something went wrong: ' + err);
-        //} else {
-        //    console.log(data.ParsedText);
-        //    
-        //}
 
         var xmlHttp = new XMLHttpRequest();
         xmlHttp.open( "GET", myURL, false ); // false for synchronous request
@@ -89,57 +65,7 @@ function setupUI(){
         
         const text = document.querySelector("#textArea");
         text.value = response.ParsedResults[0].ParsedText;
-        //var xmlHttp = new XMLHttpRequest();
-        //xmlHttp.onreadystatechange = function() { 
-        //    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-        //        callback(xmlHttp.responseText);
-        //}
-        //xmlHttp.open("GET", myURL, true); // true for asynchronous 
-        //xmlHttp.send(null);
     }
  
-}
-function Cloudmersive(){
-    
-    let defaultClient = cloudmersiveOcrApiClient.ApiClient.instance;
- 
-    // Configure API key authorization: Apikey
-    let Apikey = defaultClient.authentications['Apikey'];
-    Apikey.apiKey = "cc38c99e-3f64-41a0-9ec4-531007206dbe";
-    let api = new cloudmersiveOcrApiClient.ImageOcrApi()
- 
-    
-    let imageFile = document.querySelector("#urlArea").innerHTML; // {File} Image file to perform OCR on.  Common file formats such as PNG, JPEG are supported.
-    
- 
-    let opts = { 
-        'recognitionMode': "Basic",
-        'language': "ENG", 
-        'preprocessing': "Auto"
-    };
-
-    let callback = function(error, data, response) {
-        if (error) {
-            console.error(error);
-        } else {
-            console.log('API called successfully. Returned data: ' + data);
-        }
-    };
-    api.imageOcrPost(imageFile, opts, callback);
-}
-function OCR(){
-    //Prepare form data
-    let imgURL = document.querySelector("#urlArea");
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() 
-    {
-        if (this.readyState == 4 && this.status == 200) 
-        {
-            // Typical action to be performed when the document is ready:
-            document.querySelector("#urlArea").innerHTML = xhttp.responseText;
-        }
-    };
-    xhttp.open("GET", "filename", true);
-    xhttp.send();
 }
 export {init};
